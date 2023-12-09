747e8c867369f13bf41478e23e1497ca72c4273b
Josh Redding <josh.redding@icloud.com>
2023-12-09 14:38:57 -0700

    Adding sample .zshrc, updating zsh install notes

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -1,16 +1,12 @@
 # To install zsh, with powerline add-ons:
-# https://linuxhint.com/install_zsh_shell_ubuntu_1804/
-# https://dev.to/nicoh/installing-oh-my-zsh-on-ubuntu-362f
-# This is untested, just notes
-#
-apt install git zsh
+# Ref: https://linuxhint.com/install_zsh_shell_ubuntu_1804/
+# Ref: https://dev.to/nicoh/installing-oh-my-zsh-on-ubuntu-362f
 
+sudo apt install git zsh
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sudo chsh -s $(which zsh) $(whoami)
-#OR
-sudo usermod -s /usr/bin/zsh $(whoami)
 
 sudo apt-get install powerline fonts-powerline
 
@@ -19,3 +15,5 @@ sudo apt-get install zsh-theme-powerlevel9k
 sudo apt install zsh-syntax-highlighting
 
 cd ~/.oh-my-zsh/themes && ln -s /usr/share/powerlevel9k powerlevel9k
+
+sudo reboot
